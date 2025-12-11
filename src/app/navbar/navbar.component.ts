c782fb30362748e939905d70c24f9b70937c22a1
Maxiatef <maxatef235@gmail.com>
2025-12-11 17:32:21 +0200

    some fixes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isCollapsed = true;
 
+  toggleNavbar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  closeNavbar() {
+    this.isCollapsed = true;
+  }
 }
